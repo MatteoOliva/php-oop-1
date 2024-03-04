99128f9060551d06a3210d93c9ac38e2f1d7b295
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-04 15:05:33 +0100

    feat: test se funziona il construct

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ class Production {
     public $titolo;
     public $lingua;
     public $voto;
-}
+
+    function __construct(string $titolo, string $lingua,int $voto,)
+        {
+            $this->titolo = $titolo;
+            $this->lingua = $lingua;
+            $this->voto = $voto;
+        }
+    }
+$production = new Production('pizzeria', 'italiano', 5);
+var_dump($production);
