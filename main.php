db14080299957a52f7ad2e88a652e00c1f6e7602
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-04 16:16:12 +0100

    feat: creato metodo per cui se si inserisce un voto maggiore di 10 mi restituisce sempre 10

--- main.php
+++ main.php
@@ -8,11 +8,19 @@ class Production {
         {
             $this->titolo = $titolo;
             $this->lingua = $lingua;
-            $this->voto = $voto;
+            $this->voto = $this->votoMax($voto);
+        }
+        public function votoMax($voto) {
+            if($voto >= 10) {
+                return 10;
+        } else {
+            return $voto;
         }
     }
+}
+
 $pizzeria = new Production('Pizza5Stelle', 'italiano', 5);
-$piadineria = new Production('PiadaDelCentro', 'turco', 6);
+$piadineria = new Production('PiadaDelCentro', 'turco', 25);
 $pasticceria = new Production('BiscottiNelSacco', 'inglese', 5);
 
 $productions = [
