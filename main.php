055a7d60345032cd9f2119d431cb4da0a1036866
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-10 17:22:35 +0100

    feat: creazione classe Movie e TVSerie

--- main.php
+++ main.php
@@ -1,23 +1,54 @@
 <?php
-class Production {
+class Production
+{
     public $titolo;
     public $lingua;
     public $voto;
 
-    function __construct(string $titolo, string $lingua,int $voto,)
-        {
-            $this->titolo = $titolo;
-            $this->lingua = $lingua;
-            $this->voto = $this->votoMax($voto);
-        }
-        public function votoMax($voto) {
-            if($voto >= 10) {
-                return 10;
+    function __construct(string $titolo, string $lingua, int $voto, )
+    {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $this->votoMax($voto);
+    }
+    public function votoMax($voto)
+    {
+        if ($voto >= 10) {
+            return 10;
         } else {
             return $voto;
         }
     }
 }
+class Movie extends Production
+{
+    public $profitti;
+    public $durata;
+
+    function __construct(string $titolo, string $lingua, int $voto, int $profitti, int $durata)
+    {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $this->votoMax($voto);
+        $this->profitti = $profitti;
+        $this->durata = $durata;
+    }
+}
+
+class TVSerie extends Production
+{
+    public $numero_stagioni;
+    function __construct(string $titolo, string $lingua, int $voto, $numero_stagioni)
+    {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $this->votoMax($voto);
+        $this->numero_stagioni = $numero_stagioni;
+    }
+}
+
+
+
 
 $spiderman = new Production('spiderman', 'italiano', 8);
 $thor = new Production('thor', 'italiano', 25);
@@ -28,3 +59,5 @@ $productions = [
     $thor,
     $hulk,
 ];
+
+
